e0d26de4a8130af626b5df7560b4c2af73397e08
Nat-1501 <natalisoares90@gmail.com>
2022-07-25 09:03:46 -0300

    realizada o close da modal

--- src/route.js
+++ src/route.js
@@ -1,6 +1,8 @@
 const express = require('express')
 
 const route = express.Router()
+const RoomController = require('./controllers/RoomController')
+
 
 
 route.get('/', (req, res) => res.render("index"))
