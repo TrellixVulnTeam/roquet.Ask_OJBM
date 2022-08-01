1592294fce14b38947e72fd096f89b2450a2e1bc
Nat-1501 <natalisoares90@gmail.com>
2022-08-01 09:08:35 -0300

    importando banco de dados

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -1,13 +1,17 @@
+const Database = require("../db/config")
+
 module.exports = {
-    create(req, res) {
-        let roomId = 0
+    async create(req, res) {
+            const db = await Database()
+
+        let roomId
 
          /* Gera o numero da sala */
          for(var i = 0; i < 6; i++){
             i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
             roomId += Math.floor(Math.random() * 10).toString()
         }
-        
+
         res.redirect(`/room/${roomId}`)
     }
 }
