60c5c8cc03416968e7d11776100ea7bdbba334d9
Nat-1501 <natalisoares90@gmail.com>
2022-08-03 21:44:52 -0300

    botao excluir e marcar como lido funcionando

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -6,16 +6,14 @@ const modalTitle = document.querySelector('.modal h2')
 const modalDescription = document.querySelector('.modal p')
 const modalButton = document.querySelector('.modal button')
 
+
 //Pegar todos os botões que existe com a classe check
 const checkButtons = document.querySelectorAll(".actions a.check") 
 
 checkButtons.forEach(button => {
     //adicionar a escuta
     button.addEventListener("click", handleClick)
-   
 })
-
-
 
 /*Quando o botão delete for clicado ele abre a modal */
 const deleteButton = document.querySelectorAll(".actions a.delete")
@@ -23,7 +21,6 @@ const deleteButton = document.querySelectorAll(".actions a.delete")
 deleteButton.forEach(button => {
     button.addEventListener("click", (event) => handleClick(event, false))
 })
-
 
 function handleClick(event, check = true){
     event.preventDefault()
